6ab41798e27f1c3c3d2373b68bd987e31210ed4c
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2016-11-06 22:31:30 +0100

    Adding new methods to Query object
    - Query::getParsedQuery
    - Query::getParsedValue
    
    Both methods enable retrieving query value parsed like parse_str
    would. The only difference is that the array names are not mangled

--- test/QueryTest.php
+++ test/QueryTest.php
@@ -463,4 +463,67 @@ class QueryTest extends AbstractTestCase
         ], $data);
         $this->assertSame($querystring, Query::build($data));
     }
+
+    /**
+     * @dataProvider parsedQueryProvider
+     */
+    public function testParsedQuery($query, $expected)
+    {
+        $this->assertSame($expected, (new Query($query))->getParsedQuery());
+    }
+
+    public function parsedQueryProvider()
+    {
+        return [
+            [
+                'query' => '&&',
+                'expected' => [],
+            ],
+            [
+                'query' => 'arr[1=sid&arr[4][2=fred',
+                'expected' => [
+                    'arr[1' => 'sid',
+                    'arr' => ['4' => 'fred'],
+                ],
+            ],
+            [
+                'query' => 'arr1]=sid&arr[4]2]=fred',
+                'expected' => [
+                    'arr1]' => 'sid',
+                    'arr' => ['4' => 'fred'],
+                ],
+            ],
+            [
+                'query' => 'arr[one=sid&arr[4][two=fred',
+                'expected' => [
+                    'arr[one' => 'sid',
+                    'arr' => ['4' => 'fred'],
+                ],
+            ],
+            [
+                'query' => 'first=%41&second=%a&third=%b',
+                'expected' => [
+                    'first' => 'A',
+                    'second' => '%a',
+                    'third' => '%b',
+                ],
+            ],
+            [
+                'query' => 'arr.test[1]=sid&arr test[4][two]=fred',
+                'expected' => [
+                    'arr.test' => ['1' => 'sid'],
+                    'arr test' => ['4' => ['two' => 'fred']],
+                ],
+            ],
+            [
+                'query' => 'foo&bar=&baz=bar&fo.o',
+                'expected' => [
+                    'foo' => '',
+                    'bar' => '',
+                    'baz' => 'bar',
+                    'fo.o' => '',
+                ],
+            ],
+        ];
+    }
 }
