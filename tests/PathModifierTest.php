0f1568baca257418ae2419205e9472a41d944c40
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2019-12-03 08:15:30 +0100

    prepare 2.0.2 release

--- tests/PathModifierTest.php
+++ tests/PathModifierTest.php
@@ -7,7 +7,7 @@
  * @subpackage League\Uri\Components
  * @author     Ignace Nyamagana Butera <nyamsprod@gmail.com>
  * @license    https://github.com/thephpleague/uri-components/blob/master/LICENSE (MIT License)
- * @version    2.0.1
+ * @version    2.0.2
  * @link       https://github.com/thephpleague/uri-components
  *
  * For the full copyright and license information, please view the LICENSE
